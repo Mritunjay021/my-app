d12d1b273292bdb96ae6d870e317d700687f3f8f
Mritunjay021 <mrityurk21@gmail.com>
2026-01-13 17:35:59 +0530

    push

--- src/lib/realtime.ts
+++ src/lib/realtime.ts
@@ -1,22 +1,21 @@
-import {z} from "zod";
+import z from "zod/v4";
 import { redis } from "./redis";
-import {Realtime} from "@upstash/realtime";
+import { Realtime } from "@upstash/realtime";
 
 const schema = {
-    chat:{
-        message:z.object({
-            id:z.string(),
-            sender:z.string(),
-            text:z.string(),
-            timestamp:z.number(),
-            roomId:z.string(),
-            token:z.string().optional(),
+    chat: {
+        message: z.object({
+            id: z.string(),
+            sender: z.string(),
+            text: z.string(),
+            timestamp: z.number(),
+            roomId: z.string(),
+            token: z.string().optional(),
         }),
-        destroy:z.object({
-            isDestroyed:z.literal(true),
-        })
-    }
+        destroy: z.object({
+            isDestroyed: z.literal(true),
+        }),
+    },
+};
 
-}
-
-export const realtime = new Realtime({schema, redis})
+export const realtime = new Realtime({ schema, redis });
